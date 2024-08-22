fbd38df97e52d485556782060e7c4b0dfba45e1d
ARYA S <zenithria123@gmail.com>
2024-08-22 09:30:06 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (27.99%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -7,6 +7,10 @@ class Solution {
             num = num>>1;
         }
         return mask & compliment;
+        
+        //One line solution
         // return ~num & ((Integer.highestOneBit(num) - 1));
     }
 }
+
+//https://medium.com/@Harshit_Raj_14/476-number-complement-easy-leetcode-problem-full-solution-and-approach-explained-e321ac3773c0
