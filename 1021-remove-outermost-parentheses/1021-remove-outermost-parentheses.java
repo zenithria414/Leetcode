bab9b7b8352340429e9fb6ec05065765580b4416
ARYA S <zenithria123@gmail.com>
2024-08-29 22:35:38 +0530

    Time: 9 ms (24.83%), Space: 45.5 MB (6.49%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        String ans = "";
+        int o =0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                o++;
+                if(o>1){
+                    ans += '(';
+                }
+            }
+            else{
+                if(o>1){
+                    ans += ')';
+                }
+                    o--;
+                
+            }
+        }
+        return ans;
+    }
+}
