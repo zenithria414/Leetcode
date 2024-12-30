25a8dd6c220a120337cccbd7d59cefe405876792
ARYA S <zenithria123@gmail.com>
2024-12-30 14:13:09 +0530

    Time: 1 ms (99.38%), Space: 57.3 MB (27.13%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -10,5 +10,14 @@ class Solution {
             }
         }
         return max_sum;
+        // if(nums.length==1) return nums[0];
+        // int cs = 0;
+        // int ms = Integer.MIN_VALUE;
+        // for(int i=0; i<nums.length; i++){
+        //     cs += nums[i];
+        //     if(cs<0) cs=0;
+        //     ms = Math.max(ms,cs);
+        // }
+        // return ms;
     }
 }
