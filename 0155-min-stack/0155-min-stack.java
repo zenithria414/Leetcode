a3cc6f10a75a70209fa3af80ab743b40e2bc3940
ARYA S <zenithria123@gmail.com>
2025-06-20 15:33:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,40 +1,77 @@
-class MinStack {
-    Stack<Pair> st;
+class CustomStack {
+    int[] stack;
+    int[] inc;
+    int maxSize;
+    int top;
 
-    public MinStack() {
-        st = new Stack();
+    public CustomStack(int maxSize) {
+        this.maxSize = maxSize;
+        this.stack = new int[maxSize];
+        this.inc = new int[maxSize];
+        this.top = -1;
     }
-    
-    public void push(int val) {
-        if(st.isEmpty()){
-            st.push(new Pair(val, val));
+
+    public void push(int x) {
+        if (top + 1 >= maxSize) return;
+        top++;
+        stack[top] = x;
+    }
+
+    public int pop() {
+        if (top == -1) return -1;
+        int res = stack[top] + inc[top];
+        if (top > 0) inc[top - 1] += inc[top]; // pass down the increment
+        inc[top] = 0;
+        top--;
+        return res;
+    }
+
+    public void increment(int k, int val) {
+        int idx = Math.min(k, top + 1) - 1;
+        if (idx >= 0) {
+            inc[idx] += val;
         }
-        else{
-            st.push(new Pair(val, Math.min(st.peek().second, val)));
-        }
-    }
-    
-    public void pop() {
-        st.pop();
-    }
-    
-    public int top() {
-        return st.peek().first;
-    }
-    
-    public int getMin() {
-        return st.peek().second;
     }
 }
 
-class Pair{
-    int first, second;
 
-    Pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
+// class MinStack {
+//     Stack<Pair> st;
+
+//     public MinStack() {
+//         st = new Stack();
+//     }
+    
+//     public void push(int val) {
+//         if(st.isEmpty()){
+//             st.push(new Pair(val, val));
+//         }
+//         else{
+//             st.push(new Pair(val, Math.min(st.peek().second, val)));
+//         }
+//     }
+    
+//     public void pop() {
+//         st.pop();
+//     }
+    
+//     public int top() {
+//         return st.peek().first;
+//     }
+    
+//     public int getMin() {
+//         return st.peek().second;
+//     }
+// }
+
+// class Pair{
+//     int first, second;
+
+//     Pair(int first, int second){
+//         this.first = first;
+//         this.second = second;
+//     }
+// }
 
 /**
  * Your MinStack object will be instantiated and called as such:
