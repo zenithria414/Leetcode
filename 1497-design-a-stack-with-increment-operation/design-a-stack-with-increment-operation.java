0c15493fa2264e0d27e519170ec1fb36148b6c73
ARYA S <zenithria123@gmail.com>
2025-06-20 15:34:21 +0530

    Time: 5 ms (91.78%) | Memory: 45.9 MB (38.90%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
@@ -1,29 +1,67 @@
+//Lazy Increment method:
 class CustomStack {
-    Stack<Integer> st;
+    int[] stack;
+    int[] inc;
     int maxSize;
+    int top;
+
     public CustomStack(int maxSize) {
-        st = new Stack<>();
         this.maxSize = maxSize;
+        this.stack = new int[maxSize];
+        this.inc = new int[maxSize];
+        this.top = -1;
     }
-    
+
     public void push(int x) {
-        if(st.size()>=maxSize) return;
-        st.push(x);
+        if (top + 1 >= maxSize) return;
+        top++;
+        stack[top] = x;
     }
-    
+
     public int pop() {
-        if(st.isEmpty()) return -1;
-        int top = st.pop();
-        return top;
+        if (top == -1) return -1;
+        int res = stack[top] + inc[top];
+        if (top > 0) inc[top - 1] += inc[top]; // pass down the increment
+        inc[top] = 0;
+        top--;
+        return res;
     }
-    
+
     public void increment(int k, int val) {
-        int limit = Math.min(k,st.size());
-        for(int i=0; i<limit; i++){
-            st.set(i, st.get(i)+val);
+        int idx = Math.min(k, top + 1) - 1;
+        if (idx >= 0) {
+            inc[idx] += val;
         }
     }
 }
+
+
+// class CustomStack {
+//     Stack<Integer> st;
+//     int maxSize;
+//     public CustomStack(int maxSize) {
+//         st = new Stack<>();
+//         this.maxSize = maxSize;
+//     }
+    
+//     public void push(int x) {
+//         if(st.size()>=maxSize) return;
+//         st.push(x);
+//     }
+    
+//     public int pop() {
+//         if(st.isEmpty()) return -1;
+//         int top = st.pop();
+//         return top;
+//     }
+    
+//     public void increment(int k, int val) {
+//         int limit = Math.min(k,st.size());
+//         for(int i=0; i<limit; i++){
+//             st.set(i, st.get(i)+val);
+//         }
+//     }
+// }
 
 /**
  * Your CustomStack object will be instantiated and called as such:
