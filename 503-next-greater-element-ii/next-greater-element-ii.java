50c97d078e18cfbf80833e31b5553ba929026c63
ARYA S <zenithria123@gmail.com>
2025-06-22 19:35:12 +0530

    Time: 10 ms (85.57%) | Memory: 46.9 MB (6.78%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -3,24 +3,16 @@ class Solution {
         int n = nums.length;
         int[] res = new int[n];
         Stack<Integer> s = new Stack<>();
-
-        // Initialize result with -1
-        Arrays.fill(res, -1);
-
-        // Loop twice to simulate circular behavior
-        for (int i = 0; i < 2 * n; i++) {
-            int current = nums[i % n];
-
-            while (!s.isEmpty() && nums[s.peek()] < current) {
+        Arrays.fill(res,-1);
+        for(int i=0; i<2*n; i++){
+            int current = nums[i%n];
+            while(!s.isEmpty() && nums[s.peek()] < current){
                 res[s.pop()] = current;
             }
-
-            // Push only during the first pass to avoid duplicate pushes
-            if (i < n) {
+            if(i<n){
                 s.push(i);
-            }
-        }
-
+            }    
+        } 
         return res;
     }
 }
