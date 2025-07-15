47c6f7ed3db73e0f9c2f98f624df16acb5c0c2e0
ARYA S <zenithria123@gmail.com>
2025-07-15 20:15:59 +0530

    Time: 6 ms (100.00%) | Memory: 44.8 MB (59.77%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,30 +8,39 @@
  * }
  */
 class Solution {
-    public boolean getPath(TreeNode root, TreeNode n, ArrayList<TreeNode> path){
-        if(root == null) return false;
-        path.add(root);
-        if(root.val == n.val) return true;
+    // public boolean getPath(TreeNode root, TreeNode n, ArrayList<TreeNode> path){
+    //     if(root == null) return false;
+    //     path.add(root);
+    //     if(root.val == n.val) return true;
 
-        boolean foundLeft = getPath(root.left, n, path);
-        boolean foundRight = getPath(root.right, n, path);
+    //     boolean foundLeft = getPath(root.left, n, path);
+    //     boolean foundRight = getPath(root.right, n, path);
 
-        if(foundLeft || foundRight) return true;
-        path.remove(path.size()-1);
-        return false;
+    //     if(foundLeft || foundRight) return true;
+    //     path.remove(path.size()-1);
+    //     return false;
 
-    }
+    // }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> p1 = new ArrayList<>();
-        ArrayList<TreeNode> p2 = new ArrayList<>();
-        getPath(root, p, p1);
-        getPath(root, q, p2);
-
-        int i=0;
-        for(; i<p1.size() && i<p2.size(); i++){
-            if(p1.get(i)!=p2.get(i)) break;
+        if(root == null || root.val == p.val || root.val == q.val){
+            return root;
         }
-        TreeNode lca = p1.get(i-1);
-        return lca;
+        TreeNode leftlca = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightlca = lowestCommonAncestor(root.right, p, q);
+        if(leftlca == null) return rightlca;
+        if(rightlca == null) return leftlca;
+        return root;
+
+        // ArrayList<TreeNode> p1 = new ArrayList<>();
+        // ArrayList<TreeNode> p2 = new ArrayList<>();
+        // getPath(root, p, p1);
+        // getPath(root, q, p2);
+
+        // int i=0;
+        // for(; i<p1.size() && i<p2.size(); i++){
+        //     if(p1.get(i)!=p2.get(i)) break;
+        // }
+        // TreeNode lca = p1.get(i-1);
+        // return lca;
     }
 }
